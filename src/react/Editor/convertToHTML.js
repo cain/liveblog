76c52b1b4ae140360ca1f5fc6cc6d725b11e73ca
Erik Panchenko <erik.panchenko@news.com.au>
2018-07-06 16:20:16 +1000

    MTL-782 Added no image stretch for images narrower than 317px

--- src/react/Editor/convertToHTML.js
+++ src/react/Editor/convertToHTML.js
@@ -29,10 +29,22 @@ export default contentState =>
         }
 
         if (type === 'media') {
-          const ratio = Math.round((entity.getData().height / entity.getData().width) * 100) / 100;
+          const imgWidth = entity.getData().width;
+          const imgHeight = entity.getData().height;
+          const ratio = Math.round((imgHeight / imgWidth) * 100) / 100;
           const padding = `calc(${ratio}*100%)`;
-          return <figure style={{ margin: 0, paddingBottom: padding, position: 'relative' }}>
-            <img style={{ position: 'absolute', width: '100%', height: '100%' }} src={entity.getData().image} /></figure>;
+          let figureStyle = '';
+          let imgStyle = '';
+          if (imgWidth > 317) {
+            figureStyle = { margin: 0, paddingBottom: padding, position: 'relative' };
+            imgStyle = { position: 'absolute', width: '100%', height: '100%' };
+          } else {
+            figureStyle = { margin: 0, padding: 0, height: imgHeight, textAlign: 'center' };
+            imgStyle = { width: imgWidth, height: imgHeight };
+          }
+          const imgTag = <figure style={figureStyle}>
+            <img style={imgStyle} src={entity.getData().image} /></figure>;
+          return imgTag;
         }
       }
       if (block.type === 'unordered-list-item') {
